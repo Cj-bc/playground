ab58b4f60109e1765ecce919c070a8a59eb1b219
Cj-bc <cj-bc@y-modify.org>
2019-01-15 12:46:58 +0900

    Add: cpp> 12> some codes

---
+++ cpp/12_6_check_the_number_of_significant_figures.cpp
@@ -0,0 +1,8 @@
+
+
+int main()
+{
+  std::cout << "float: " << std::numeric_limits<float>::digits10 << "\n"
+            << "double: " << std::numeric_limits<double>::digits10 << "\n"
+            << "long double: " << std::numeric_limits<long double>::digits10 << "\n";
+}
