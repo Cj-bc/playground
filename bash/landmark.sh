084bb8c213e853903e86497586ab8d3383406da3
Cj-bc <cj-bc@y-modify.org>
2019-02-09 22:07:06 +0900

    Add: bash> landmark.sh
    Landmark the speed.
    repeat command for 1,000 times, and display result.

---
+++ bash/landmark.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Copyright 2019 (c) Cj-bc
+# This software is released under MIT License
+#
+# @(#) version -
+
+# usage: ./landmark.sh <target command> <comamnd to compair>
+#
+
+readonly target="$1"
+readonly compair_to="$2"
+
+echo "execute target for 1,000 times: ${target}"
+{ time \
+  for i in {0..1000};do
+    $target
+  done; } >/dev/null
+
+echo "execute compair to for 1,000 times: ${compair_to}"
+{ time \
+  for i in {0..1000};do
+    $compair_to
+  done; } >/dev/null
