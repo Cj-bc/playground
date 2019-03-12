da8650388415b902654c0c8a47e35652249da6c4
Cj-bc <cj-bc@y-modify.org>
2019-03-13 01:38:56 +0900

    Add: haskell> 7shi> 6_perpPoint> just a stash of WIP

---
+++ haskell/qiita_7shi_haskell_intro/6_perpPoint.hs
@@ -0,0 +1,12 @@
+-- original     : ax + by = c
+-- perpendicular: bx - ay = d
+
+perpPoint (p, q) (a, b, c) = 
+d = b*x - a*y
+
+ax + by = c
+     by = -ax + c
+      y = -(a/b)*x + c/b
+
+      y' = b/a*x' + n
+      y' = (a/b)*x' + c/b
