82b06e663f8d45fd7acb7c7a7571466f1dc7b398
Cj-bc <cj-bc@y-modify.org>
2019-03-13 15:03:09 +0900

    Add: haskell> 7shi> 6> complete perpPoint function

--- haskell/qiita_7shi_haskell_intro/6_perpPoint.hs
+++ haskell/qiita_7shi_haskell_intro/6_perpPoint.hs
@@ -1,12 +1,20 @@
 -- original     : ax + by = c
 -- perpendicular: bx - ay = d
+--
+-- Calculate 'd' by inserting (p,q) to (x,y)
+--
+--    ax      + by      = c
+-- -) b(a/b)x - a(a/b)y = d(a/b)
+-- -----------------------------
+--        {b + a(a/b)}y = c - d(a/b)
+--                    y = {c - d(a/b)}/{b + a(a/b)}
+--
+-- Push y to 'bx - ay = d'
 
-perpPoint (p, q) (a, b, c) = 
-d = b*x - a*y
+perpPoint (p, q) (a, b, c) = (x, y)
+  where d = b*p - a*q
+        y = (c - d*(a/b))/(b + a*(a/b))
+        x = (d + a*y) / b
 
-ax + by = c
-     by = -ax + c
-      y = -(a/b)*x + c/b
-
-      y' = b/a*x' + n
-      y' = (a/b)*x' + c/b
+main = do
+  print $ perpPoint (1, 1) (1, 1, 1)
