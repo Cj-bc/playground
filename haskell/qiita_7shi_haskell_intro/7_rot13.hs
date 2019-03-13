6a0ea10640d66cae2fea634ad79135d857d23973
Cj-bc <cj-bc@y-modify.org>
2019-03-13 23:04:36 +0900

    Change: haskell> 7shi> 7> re-write based on example

--- haskell/qiita_7shi_haskell_intro/7_rot13.hs
+++ haskell/qiita_7shi_haskell_intro/7_rot13.hs
@@ -1,10 +1,15 @@
 import Data.Char
 
+
+rot13ch :: Char -> Char
+rot13ch ch |  'A' <= ch && ch <= 'M'
+           || 'a' <= ch && ch <= 'm' = chr $ ord ch +13
+           |  'N' <= ch && ch <= 'Z'
+           || 'n' <= ch && ch <= 'z' = chr $ ord ch -13
+           | otherwise               = ch
+
 rot13 :: String -> String
-rot13 (x:xs) | xs == []  = [shifted]
-             | otherwise = shifted : rot13 xs
-  where
-    shifted = chr (origin+13)
-    origin  = ord x
+rot13 ""      = ""
+rot13 (x:xs)  =  rot13ch x : rot13 xs
 
 main = print $ rot13 "abc"
