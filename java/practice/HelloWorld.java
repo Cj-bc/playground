cd6f7cd36706d62507b9e46e99cbc6f56cd8c1bc
Cj-bc <cj-bc@y-modify.org>
2019-03-16 19:05:19 +0900

    Add: java> practice> HelloWorld

---
+++ java/practice/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "Hello World" to stdout.
+ * https://docs.oracle.com/javase/tutorial/getStarted/cupojava/unix.html
+*/
+class HelloWorld {
+  public static void main(String[] args) {
+   System.out.println("Hello World");
+  }
+}
