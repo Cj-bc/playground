8e2a67dc6d6b0c9499200d0cfdf40a855f6d5165
Cj-bc <cj-bc@y-modify.org>
2019-03-18 10:27:00 +0900

    Change: haskell?7shi> 11> add example solution

--- haskell/qiita_7shi_haskell_intro/11_triangle_each_edge_less_than_20.hs
+++ haskell/qiita_7shi_haskell_intro/11_triangle_each_edge_less_than_20.hs
@@ -9,4 +9,10 @@ isTriangle (x, y, z) | x^2 + y^2 == z^2 = True
 
 
 main = do
-  putStrLn $ show [ t | t <- mkTupleOfEdges, isTriangle t]
+  -- The First one is my solution,
+  -- The second one is example solution.
+  putStrLn $ "my solution:" ++ show [ t | t <- mkTupleOfEdges, isTriangle t]
+  putStrLn $ "sample solution:" ++ show [ (x, y, z) | x <- [1..20], y <- [1..20], z <- [1..20],
+                                x^2 + y^2 == z^2 ||
+                                x^2 + z^2 == y^2 ||
+                                y^2 + z^2 == x^2 ]
