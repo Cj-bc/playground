f64ba16012aaa345c57b7433302b2776e24c528d
Cj-bc <cj-bc@y-modify.org>
2019-03-18 10:37:33 +0900

    Add: haskell> 7shi> 2_1> add 'White' based on example solution

--- haskell/qiita_7shi_haskell_intro/2_1_three_colors_and_mix.hs
+++ haskell/qiita_7shi_haskell_intro/2_1_three_colors_and_mix.hs
@@ -1,4 +1,4 @@
-data Color = Red | Blue | Green | Yellow | Cyan | Magenta
+data Color = Red | Blue | Green | Yellow | Cyan | Magenta | White
               deriving (Show, Enum, Eq)
 
 mix :: Color -> Color -> Color
@@ -6,6 +6,12 @@ mix first second  | first == second = first
                   | (fromEnum first) + (fromEnum second) == 1 = Magenta
                   | (fromEnum first) + (fromEnum second) == 2 = Yellow
                   | (fromEnum first) + (fromEnum second) == 3 = Cyan
+                  | (fromEnum first) + (fromEnum second) >= 7 = White
+                  | first == Magenta && second == Green       = White
+                  | first == Yellow  && second == Blue        = White
+                  | first == Cyan    && second == Red         = White
+                  | otherwise       = mix second first
 
 main = do
   putStrLn $ show $ mix Red Blue
+  putStrLn $ show $ mix Red Cyan
