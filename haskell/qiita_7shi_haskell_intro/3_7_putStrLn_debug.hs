cdf0ac00b8cf3175b99d436cbaff86b7fe7dd6b3
Cj-bc <cj-bc@y-modify.org>
2019-03-20 14:35:26 +0900

    Add: haskell> 7shi> 3_7> my solution

---
+++ haskell/qiita_7shi_haskell_intro/3_7_putStrLn_debug.hs
@@ -0,0 +1,16 @@
+qsort :: Show a => Ord a => [a] -> IO [a]
+qsort [] = do
+  putStrLn "qsort [] = []"
+  return []
+qsort (n:xs) = do
+    let lt    = [x | x <- xs, x < n]
+    let gteq  = [x | x <- xs, x >= n]
+    putStrLn $ "qsort " ++ show (n:xs) ++ " = qsort " ++ show lt ++ " ++ " ++ show [n] ++ " ++ " ++ show gteq
+    lt'   <- qsort lt
+    gteq' <- qsort gteq
+    putStrLn $ "qsort " ++ show (n:xs) ++ " = qsort " ++ show lt ++ " ++ " ++ show [n] ++ " ++ " ++ show gteq
+    return $ lt' ++ [n] ++ gteq'
+
+
+main = do
+  print =<< qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
