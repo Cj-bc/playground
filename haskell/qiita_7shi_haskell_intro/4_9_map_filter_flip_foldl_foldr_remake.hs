3e7a16cd222e59a480f98bee54b849975aeb0d32
Cj-bc <cj-bc@y-modify.org>
2019-04-08 00:20:42 +0900

    Change: haskell/7shi/4_9> WIP

--- haskell/qiita_7shi_haskell_intro/4_9_map_filter_flip_foldl_foldr_remake.hs
+++ haskell/qiita_7shi_haskell_intro/4_9_map_filter_flip_foldl_foldr_remake.hs
@@ -8,16 +8,41 @@ filter' :: (a -> Bool) -> [a] -> [a]
 filter' fn xs = [x | x <- xs, fn x]
 
 -- not yet
+-- foldl' (\x -> \y -> x^y) 2 [1,2,3,4,5]
+-- (foldl' (\x -> \y -> x^y) 2 [1,2,3,4])^5
+-- ((foldl' (\x -> \y -> x^y) 2 [1,2,3])^4)^5
+-- (((foldl' (\x -> \y -> x^y) 2 [1,2])^3)^4)^5
+-- ((((foldl' (\x -> \y -> x^y) 2 [1])^2)^3)^4)^5
+-- (((((foldl' (\x -> \y -> x^y) 2 [])^1)^2)^3)^4)^5
+-- (((((2)^1)^2)^3)^4)^5
 foldl' :: (a -> a -> a) -> a -> [a] -> a
 foldl' fn ini []      = ini
-foldl' fn ini (x:xs)  = foldl' fn done xs
+foldl' fn ini xs  = fn (foldl' fn ini rest) lx
     where
-        done = fn ini x
+        lx = last xs
+        rest = init xs
+
 
 -- not yet
+-- foldr (\x -> \y -> x^y) 2 [1,2,3,4,5]
+--  1^(foldr (\x -> \y -> x^y) 2 [2,3,4,5])
+--  1^(2^(foldr (\x -> \y -> x^y) 2 [3,4,5]))
+--  1^(2^(3^(foldr (\x -> \y -> x^y) 2 [4,5])))
+--  1^(2^(3^(4^(foldr (\x -> \y -> x^y) 2 [5]))))
+--  1^(2^(3^(4^(5^(foldr (\x -> \y -> x^y) 2 [])))))
+--  1^(2^(3^(4^(5^(2)))))
 foldr' :: (a -> a -> a) -> a -> [a] -> a
-foldr' fn ini xs = foldr' fn done xs'
+foldr' fn ini [] = ini
+foldr' fn ini xs = fn first (foldr' fn ini rest)
     where
-        last' = last xs
-        done  = fn ini last'
-        xs'   = 
+        first = head xs
+        rest  = drop 1 xs
+
+
+main = do
+  print $ map' (* 2) [1..5]
+  print $ filter' (< 5) [1..9]
+  print $ flip' map' [1..5] (* 2)
+  print $ foldl' (+) 0 [1..100]
+  print $ foldl' (-) 0 [1..5]
+  print $ foldr' (-) 0 [1..5]
