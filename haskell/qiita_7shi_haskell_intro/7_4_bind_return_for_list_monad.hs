fd25b74ef8bcf5f081467cb01ead3fd34f5dac9b
Cj-bc <cj-bc@y-modify.org>
2019-04-11 21:25:42 +0900

    Fix: haskell/7shi/7_4> according to example

--- haskell/qiita_7shi_haskell_intro/7_4_bind_return_for_list_monad.hs
+++ haskell/qiita_7shi_haskell_intro/7_4_bind_return_for_list_monad.hs
@@ -1,7 +1,6 @@
-bind xs fn = foldr fn [] xs
+bind xs fn = foldr ((++) . fn) [] xs
 
 return' x = [x]
 
 main = do
---  print $ [1..3] `bind` \x -> "abc" `bind` \y -> return' (x, y)
-  print $ bind [1..3] (\x -> bind "abc" (\y -> return' (x, y)))
+  print $ [1..3] `bind` \x -> "abc" `bind` \y -> return' (x, y)
