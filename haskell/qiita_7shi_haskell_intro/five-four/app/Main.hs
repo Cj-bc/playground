1149780223aff82bd4318cc24a34992ad19ba20f
Cj-bc <cj-bc@y-modify.org>
2019-04-11 12:37:35 +0900

    Fix: haskell/7shi/five-four> according to example

--- haskell/qiita_7shi_haskell_intro/five-four/app/Main.hs
+++ haskell/qiita_7shi_haskell_intro/five-four/app/Main.hs
@@ -1,9 +1,13 @@
 import System.Random
 import Control.Monad
 
-round' x = if (x - truncated) > 0.5 then truncated else truncated + 1
-  where
-    truncated = fromIntegral $ truncate x
+-- mine {{{
+-- round' x = if (x - truncated) > 0.5 then truncated else truncated + 1
+--   where
+--     truncated = fromIntegral $ truncate x
+--     }}}
+round' x = truncate (x + 0.5)
+-- Nicer way to define round.
 
 main :: IO ()
 main = do
@@ -11,7 +15,15 @@ main = do
               sum <$> replicateM 12 (getStdRandom (randomR (0.0, 1.0)) :: IO Double)
   let xs' = flip map xs $ \x -> (round' x) - 6
 
+--  mine {{{
+--  forM_ [-3..3] $ \x -> do
+--        putStr $ reverse (drop 2 (reverse (show x))) ++ ": "
+--        replicateM (length [n | n <- xs', n == x]) $ putStr "*"
+--        putStrLn " "
+--        }}}
   forM_ [-3..3] $ \x -> do
-        putStr $ reverse (drop 2 (reverse (show x))) ++ ": "
-        replicateM (length [n | n <- xs', n == x]) $ putStr "*"
-        putStrLn " "
+      let c = length $ filter (== x) xs'
+      let x' = show x
+      let n = replicate (2 - length x') ' ' ++ x'
+      putStrLn $ n ++ ": " ++ replicate c '*'
+-- Specify padding by using replicate (n)
