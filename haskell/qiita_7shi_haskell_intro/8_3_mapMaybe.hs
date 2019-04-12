6ead3eaf22b75b1975539876c013caa1becb24f7
Cj-bc <cj-bc@y-modify.org>
2019-04-12 15:34:28 +0900

    fix: haskell/7shi/8_3> fix typo

--- haskell/qiita_7shi_haskell_intro/8_3_mapMaybe.hs
+++ haskell/qiita_7shi_haskell_intro/8_3_mapMaybe.hs
@@ -12,8 +12,8 @@ fact 0 = Just 1
 fact n  | n > 0     = (n *) <$> fact (n - 1)
         | otherwise = Nothing
 
-facts n = ( map       fact [n, n - 1. n - 2],
-            mapMaybe'  fact [n, n - 1. n - 2])
+facts n = ( map       fact [n, n - 1, n - 2],
+            mapMaybe'  fact [n, n - 1, n - 2])
 
 main = do
   print $ facts 3
