9d88b38dd9a1973e509f32a172e6bec1ac124426
Cj-bc <cj-bc@y-modify.org>
2019-04-14 19:01:42 +0900

    Fix: haskell/7shi/8_4> according to sample

--- haskell/qiita_7shi_haskell_intro/8_4_mapMaybe_with_foldr.hs
+++ haskell/qiita_7shi_haskell_intro/8_4_mapMaybe_with_foldr.hs
@@ -1,8 +1,26 @@
 import Control.Applicative
 
+-- mine {{{
+-- mapMaybe :: (a -> Maybe b) -> [a] -> [b]
+-- mapMaybe _ [] = []
+-- mapMaybe f (x:xs) = filter (/= Nothing) $ foldr ((++) . f) [] xs
+-- }}}
 mapMaybe :: (a -> Maybe b) -> [a] -> [b]
-mapMaybe _ [] = []
-mapMaybe f (x:xs) = filter (/= Nothing) $ foldr ((++) . f) [] xs
+mapMaybe f = (`foldr` []) $ \x xs -> case f x of
+  Just x' -> x' : xs
+  Nothing -> xs
+-- (`foldr` [])
+--    |  As foldr is used like 'infix function',
+--    | '[]' is 2nd argument of 'foldr'.
+--    |  The first argument is actually missing
+--    | so that it'll take it from backward.
+--
+-- (`foldr` []) $ \x xs -> ...
+--    |  '$ \x xs ...' is the first argument of
+--    | 'foldr'.
+--
+-- The last argument is supplied as 
+
 
 fact 0 = Just 1
 fact n  | n > 0     = (n *) <$> fact (n - 1)
