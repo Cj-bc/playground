f9707d3914acb4fe747263f98c1904880b6310b4
Cj-bc <cj-bc@y-modify.org>
2019-04-21 12:10:32 +0900

    Change: haskell/haskell.org/hitchhikers/cd-fit> parsers are implemented

--- haskell/haskell.org/hitchhikers_guide_to_haskell/cd-fit/cd-fit.hs
+++ haskell/haskell.org/hitchhikers_guide_to_haskell/cd-fit/cd-fit.hs
@@ -5,6 +5,26 @@
 
 
 module Main where
+import Text.ParserCombinators.Parsec
+
+-- parseInput parses output of "du -sb", which consists of many lines.
+-- each of which describes single directory
+parseInput = do
+        dirs <- many dirAndSize
+        eof :: Parser ()
+        return dirs
+
+-- Datatype Dir holds information about single directory - its size and name
+data Dir = Dir Int String deriving Show
+
+-- `dirAndSize` parses information about single directory, which is:
+-- a size in bytes (number), some spaces then directory name, which extends till newline
+dirAndSize = do
+        size <- many1 digit
+        spaces
+        dir_name <- anyChar `manyTill` newline
+        return (Dir (read size) dir_name)
+
 
 main = do
         input <- getContents
