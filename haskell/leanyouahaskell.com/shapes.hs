0df3db0cb0051cb99703004df8a271de3b1160b8
Cj-bc <cj-bc@y-modify.org>
2019-05-06 14:16:20 +0900

    Add: haskell/leanyouahaskell/shapes> nudge

--- haskell/leanyouahaskell.com/shapes.hs
+++ haskell/leanyouahaskell.com/shapes.hs
@@ -5,11 +5,17 @@ surface :: Shape -> Float
 surface (Circle _ l) = pi * l * l
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = abs $ (x2 - x1) * (y2 - y1)
 
-
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) l)    mx my = Circle (Point (x + mx) (y + my)) l
+nudge (Rectangle (Point x y) (Point x2 y2)) mx my = Rectangle (Point (x + mx) (y + my)) (Point (x2 + mx) (y2 + my))
 
 
 main = do
         let rec = Rectangle (Point 0 0) (Point 10 10)
             cir = Circle (Point 0 0) 10.0
+        putStrLn $ "surface of " ++ (show rec) ++ " | " ++ (show cir)
         print $ surface rec
         print $ surface cir
+        putStrLn $ "nudge them by (10, 10)"
+        print $ nudge rec 10 10
+        print $ nudge cir 10 10
