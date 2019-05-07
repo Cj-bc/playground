c7695947ad03920605cdf52aca24c79df1127ad7
Cj-bc <cj-bc@y-modify.org>
2019-05-07 16:38:06 +0900

    Add: haskell/leanyouahaskell> todolist

---
+++ haskell/leanyouahaskell.com/todolist/todo.hs
@@ -0,0 +1,38 @@
+import Data.Char
+import System.IO
+
+main = do
+        putStrLn $ unlines ["=============== TODO LIST ==============="
+                           ,"1) List your TODO from \"todo.txt\""
+                           ,"2) Add todo"
+                           ,"3) Remove todo"]
+        putStr "Your choice: "
+        choice <- getLine
+        case (read choice :: Int) of
+                1 -> listTodo
+                2 -> do
+                  title <- prompt "Title: "
+                  addTodo title
+                3 -> do
+                  num <- prompt "Which to remove: "
+                  removeTodo num
+                _ -> return ()
+
+
+prompt :: String -> IO String
+prompt message = do
+        putStr message
+        answer <- getLine
+        return answer
+
+
+addTodo :: String -> IO ()
+addTodo title = appendFile "todo.txt" $ title ++ "\n"
+
+removeTodo :: String -> IO ()
+removeTodo number = putStrLn "Not implemented."
+
+listTodo :: IO ()
+listTodo = do
+        contents <- readFile "todo.txt"
+        putStr contents
