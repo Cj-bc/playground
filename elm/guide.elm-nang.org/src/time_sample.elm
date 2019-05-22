c89e5089b03aeb0a5d7c1ed71bfddc4410dd9063
Cj-bc <cj-bc@y-modify.org>
2019-05-22 23:04:57 +0900

    Change: Trying to use SVG to display analog clock [WIP]

--- elm/guide.elm-nang.org/src/time_sample.elm
+++ elm/guide.elm-nang.org/src/time_sample.elm
@@ -3,6 +3,12 @@ import Html exposing (..)
 import Html.Events exposing (onClick)
 import Task
 import Time
+import Svg exposing (line,circle,svg)
+import Svg.Attributes exposing (cy, cx, r
+                               ,x1, y1, x2, y2
+                               ,x, y, width, height
+                               , fill, stroke, strokeWidth
+                               )
 
 -- MAIN
 main =
@@ -62,6 +68,75 @@ view model =
     let hour = String.fromInt (Time.toHour model.zone model.time)
         minute = String.fromInt (Time.toMinute model.zone model.time)
         second = String.fromInt (Time.toSecond model.zone model.time)
-    in div [] [text (hour ++ " : " ++ minute ++ " : " ++ second)
+    in div [] [svgClock model
+              ,text (hour ++ " : " ++ minute ++ " : " ++ second)
               ,button [ onClick TogglePause ][ text "pause"]]
 
+
+
+type alias Coordinate = {x : Float
+                        ,y : Float
+                        }
+
+type ClockHand = HourHand
+               | MinuteHand
+               | SecondHand
+
+svgClock : Model -> Html Msg
+svgClock model =
+  let hourHandAngle   = calcHandAngle HourHand model
+      minuteHandAngle = calcHandAngle MinuteHand model
+      secondHandAngle = calcHandAngle SecondHand model
+      hourHand_ext    = Coordinate (40 * (cos hourHandAngle)) (-1 * 40 * (sin hourHandAngle))
+      minuteHand_ext  = Coordinate (80 * (cos minuteHandAngle)) (-1 * 80 * (sin minuteHandAngle))
+      secondHand_ext  = Coordinate (70 * (cos secondHandAngle)) (-1 * 70 * (sin secondHandAngle))
+      clockBase   = Coordinate 100 100
+      clockFrame  = circle [ cx (String.fromFloat clockBase.x)
+                           , cy (String.fromFloat clockBase.y)
+                           , r "100"
+                           , fill "none"
+                           , stroke "black"] []
+      hourHand    = line [ x1 (String.fromFloat clockBase.x)
+                         , y1 (String.fromFloat clockBase.y)
+                         , x2 (String.fromFloat (clockBase.x + hourHand_ext.x))
+                         , y2 (String.fromFloat (clockBase.y + hourHand_ext.y))
+                         , stroke "red"
+                         , strokeWidth "5"] []
+      minuteHand  = line [ x1 (String.fromFloat clockBase.x)
+                         , y1 (String.fromFloat clockBase.y)
+                         , x2 (String.fromFloat (clockBase.x + minuteHand_ext.x))
+                         , y2 (String.fromFloat (clockBase.y + minuteHand_ext.y))
+                         , stroke "black"
+                         , strokeWidth "3"] []
+      secondHand  = line [ x1 (String.fromFloat clockBase.x)
+                         , y1 (String.fromFloat clockBase.y)
+                         , x2 (String.fromFloat (clockBase.x + secondHand_ext.x))
+                         , y2 (String.fromFloat (clockBase.y + secondHand_ext.y))
+                         , stroke "black"
+                         , strokeWidth "1"] []
+  in svg  [x "0"
+          ,y "0"
+          ,width "200"
+          ,height "200"
+          ]
+          [ clockFrame
+          , hourHand
+          , minuteHand
+          , secondHand
+          ]
+
+
+
+calcHandAngle : ClockHand -> Model -> Float
+calcHandAngle hand model =
+  let hour   = Time.toHour model.zone model.time
+      minute = Time.toMinute model.zone model.time
+      second = Time.toSecond model.zone model.time
+  in
+  case hand of
+    HourHand ->
+      toFloat (((360 // 12) * hour) + 90)
+    MinuteHand ->
+      toFloat ((2 * minute) + 90)
+    SecondHand ->
+      toFloat ((2 * second) + 90)
