e4f0665be4bf1cd9e544056c9fe10f80bb58be0b
Cj-bc <cj-bc@y-modify.org>
2019-05-31 15:06:08 +0900

    Add: elm/todo/Entry> add entryEncoder to encode 'Entry' to JSON object

--- elm/todo/src/Entry.elm
+++ elm/todo/src/Entry.elm
@@ -1,4 +1,6 @@
-module Entry exposing (Entry)
+module Entry exposing (Entry, entryEncoder)
+
+import Json.Encode as E
 
 
 type alias Entry =
@@ -7,3 +9,13 @@ type alias Entry =
     , detail : String
     , done : Bool
     }
+
+
+entryEncoder : Entry -> E.Value
+entryEncoder e =
+    E.object
+        [ ( "id", E.int e.id )
+        , ( "title", E.string e.title )
+        , ( "detail", E.string e.detail )
+        , ( "done", E.bool e.done )
+        ]
