a97cde1414ec30d5cd0c2d8b4bce44d0b65dac4b
Cj-bc <cj.bc-sd@outlook.jp>
2019-09-04 11:21:14 +0900

    Add: unity> practice project

---
+++ unity/ChickAttack/Assets/ChickGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickGenerator : MonoBehaviour
+{
+    public GameObject obj;
+    public float interval = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObj", 0.1f, interval);
+    }
+
+    void SpawnObj()
+    {
+        Instantiate(obj, transform.position, transform.rotation);
+    }
+}
---
+++ unity/ChickAttack/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 8f;
+    public float moveableRange = 5.5f;
+    public float power = 1000f;
+    public GameObject cannonBall;
+    public Transform spawnPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxisRaw(
+              "Horizontal") * speed * Time.deltaTime, 0, 0);
+        transform.position = new Vector2(Mathf.Clamp(
+              transform.position.x, -moveableRange, moveableRange),
+              transform.position.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+      GameObject newBullet = Instantiate(cannonBall, spawnPoint.position, Quaternion.identity) as GameObject;
+      newBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power);
+    }
+}
