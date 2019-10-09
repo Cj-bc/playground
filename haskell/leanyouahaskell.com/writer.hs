9b828f466264ada4ae32ec7d43c3b7b24b26fa67
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-09 11:20:56 +0900

    Add: haskell/learnyouahashell/writer> 'addDrink' [p319]

--- haskell/leanyouahaskell.com/writer.hs
+++ haskell/leanyouahaskell.com/writer.hs
@@ -1,4 +1,15 @@
+import Data.Monoid
+
 applyLog :: Monoid m => (a, m) -> (a -> (b, m)) -> (b, m)
 applyLog (x, log) f = (x' , log `mappend` newLog)
     where
         (x', newLog) = f x
+
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "Sushi"     = ("Green Tea", Sum 120)
+addDrink "Hamburger" = ("Cola", Sum 100)
+addDrink "Bread"     = ("Cocoa", Sum 100)
+addDrink _           = ("Water", Sum 10)
