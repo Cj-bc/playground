b6bb752eff834558fd61958ae6c1bb35c10f5cba
Cj-bc <cj-bc@y-modify.org>
2019-10-10 18:56:45 +0900

    Fix: haskell/learnyouahaskell/knightJourney> remove illegal coordinate (0, 0)

--- haskell/leanyouahaskell.com/knightJourney.hs
+++ haskell/leanyouahaskell.com/knightJourney.hs
@@ -11,7 +11,7 @@ moveKnight :: KnightPos -> [KnightPos]
 moveKnight (c, r) = do
     (c', r') <- [(c+1, r+2), (c+1, r-2), (c-1, r+2), (c-1, r-2)
                 ,(c+2, r+1), (c+2, r-1), (c-2, r+1), (c-2, r-1)]
-    guard (c' `elem` [0..8] && r' `elem` [0..8])
+    guard (c' `elem` [1..8] && r' `elem` [1..8])
     return (c', r')
 
 canReachIn3 :: KnightPos -> KnightPos -> Bool
