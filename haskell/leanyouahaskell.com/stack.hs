a90db46dd0fe034fffd32ea9e1d130598e947057
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-12 02:15:08 +0900

    Add: hs/lyh/stack> implementing State monad by myself [WIP]

--- haskell/leanyouahaskell.com/stack.hs
+++ haskell/leanyouahaskell.com/stack.hs
@@ -1,3 +1,26 @@
+newtype State s a = State { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+    -- prop> fmap id s == s
+    fmap h (State f) = State $ \s ->
+                            let (a, newState) = f s
+                                g = h a
+                            in (g, newState)
+
+instance Applicative (State s) where
+    pure a = State $ \s -> (a, s)
+    State h <*> State f = State $ \s ->
+                            let g = h f
+                            in ()
+
+
+instance Monad (State s) where
+    return x = State $ \s -> (x, s)
+    (State h) >>= f = State $ \s ->
+                        let (a, newState) = h s
+                            (State g) = f a
+                        in (g, newState)
+
 type Stack = [Int]
 
 pop :: Stack -> (Int, Stack)
