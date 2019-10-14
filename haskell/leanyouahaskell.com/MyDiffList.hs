1f03aeca9f11b5b953c4dccd84b2798d7ea53fdc
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-14 19:17:29 +0900

    Add: hs/lyh/MyDiffList> probe how DiffList satisfy Semigroup law

--- haskell/leanyouahaskell.com/MyDiffList.hs
+++ haskell/leanyouahaskell.com/MyDiffList.hs
@@ -7,6 +7,25 @@ toDiffList :: [a] -> DiffList a
 toDiffList xs = DiffList (xs ++)
 
 instance Semigroup (DiffList a) where
+    -- prop> (a <> b) <> c == a <> (b <> c)
+    --
+    --  (DiffList (a ++) <> DiffList (b ++)) <> DiffList (c ++)
+    --      = DiffList (\xs -> ((a ++) . (b ++)) xs) <> DiffList (c ++)
+    --      = DiffList (\xs -> (a ++ b ++ xs) <> DiffList (c ++)
+    --      = DiffList (a ++ b ++) <> DiffList (c ++)
+    --      = DiffList (\xs -> (a ++ b ++) . (c ++) xs)
+    --      = DiffList (\xs -> (a ++ b ++) (c ++ xs))
+    --      = DiffList (\xs -> a ++ b ++ c ++ xs)
+    --      = DiffList (a ++ b ++ c ++)
+    --
+    --  DiffList (a ++) <> (DiffList (b ++) <> DiffList (c ++))
+    --      = DiffList (a ++) <> (DffList (\xs -> ((b ++) . (c ++)) xs))
+    --      = DiffList (a ++) <> (DffList (\xs -> b ++ c ++ xs))
+    --      = DiffList (a ++) <> DffList (b ++ c ++)
+    --      = DiffList (\xs -> ((a ++) . (b ++ c ++)) xs)
+    --      = DiffList (\xs -> a ++ b ++ c ++ xs)
+    --      = DiffList (a ++ b ++ c ++)
+    --
     (DiffList a) <> (DiffList b) = DiffList (\xs -> (a . b) xs)
 
 instance Monoid (DiffList a) where
