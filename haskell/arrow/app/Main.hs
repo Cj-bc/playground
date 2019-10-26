d2d94c7b1aa197850be96abf810902b18f33f9ef
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-26 21:46:57 +0900

    Add: hs/arrow/main> add instances for Category, Arrow

--- haskell/arrow/app/Main.hs
+++ haskell/arrow/app/Main.hs
@@ -11,5 +11,20 @@ import System.Random
 
 newtype Circuit a b = Circuit { unCircuit :: a -> (Circuit a b, b)}
 
+instance Cat.Category Circuit where
+    id = Circuit $ \a -> (Cat.id, a)
+    (.) = dot
+        where
+            (Circuit cir2) `dot` (Circuit cir1) = Circuit $ \a ->
+                let (cir1', b) = cir1 a
+                    (cir2', c) = cir2 b
+                in (cir2' `dot` cir1', c)
+
+instance Arrow Circuit where
+    arr f = Circuit $ \a -> (arr f, f a)
+    first (Circuit cir) = Circuit $ \(a, b) ->
+        let (cir', c) = cir a
+        in (first cir', (c, b))
+
 main :: IO ()
 main = return ()
