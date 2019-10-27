413dd792f92cf0c56c5d788c457db39392e891a9
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-27 14:59:45 +0900

    Add: unity/BeginCSharpWithUnity_MdN>

---
+++ unity/BeginCSharpWithUnity_MdN/Assets/Lesson1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Person p = new Person();
+      p.lastName = "魔王";
+      p.firstName = "まぐろな";
+      p.show();
+      Debug.Log("私のフルネームは" + p.getFullName());
+    }
+}
+
+
+class Person {
+  public string firstName;
+  public string lastName;
+
+  public void show() {
+    Debug.Log("私は" + this.firstName);
+  }
+
+  public string getFullName() {
+    return lastName + firstName;
+  }
+}
