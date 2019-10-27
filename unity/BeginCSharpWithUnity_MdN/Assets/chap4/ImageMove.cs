ea058eac27f29ca41541414af1d60bbf61ccd9e1
Cj-bc <cj.bc-sd@outlook.jp>
2019-10-27 16:36:00 +0900

    Add: unity/BeginCSharpWithUnity_MdN> 'ImageMovie', which moves image randomly

---
+++ unity/BeginCSharpWithUnity_MdN/Assets/chap4/ImageMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageMove : MonoBehaviour
+{
+    Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = this.GetComponent<Transform>().position;
+        this.startPos = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform tr = this.GetComponent<Transform> ();
+        Vector3 pos = tr.position;
+        pos.x = Random.Range(-2.0f, 2.0f);
+        pos.y = Random.Range(-2.0f, 2.0f);
+        tr.position = pos - this.startPos;
+    }
+}
