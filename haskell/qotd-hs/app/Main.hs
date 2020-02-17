21f32370675b13a2a69d03438e21d266077a5552
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-17 21:28:02 +0900

    Add: hs/qotd-hs> documents

--- haskell/qotd-hs/app/Main.hs
+++ haskell/qotd-hs/app/Main.hs
@@ -1,9 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-|
+Module      : Main
+Description : main module for qotd-hs
+Copyright   : (c) Cj.bc_sd a.k.a Cj-bc, 2020
+Maintainer  : cj.bc-sd@outlook.jp
+Stability   : experimental
+
+References:
+
+  - https://tools.ietf.org/html/rfc865
+
+  - https://hackage.haskell.org/package/network-3.1.1.1/docs/Network-Socket.html
+
+  - http://agtn.hatenablog.com/entry/2015/07/23/214344
+-}
 module Main where
 
 import Control.Concurrent (forkFinally)
 import Control.Monad (void, forever)
-import Data.ByteString (ByteString(..), append)
+import Data.ByteString (ByteString, append)
 import Data.ByteString.UTF8 (fromString)
 import Network.Socket
 import Network.Socket.ByteString
@@ -16,12 +31,17 @@ main = do
     forever $ forkServerForEach sock
 
 
+-- | create socket
+--
+-- IPv4, TCP
 createSocket = do
     sock <- socket AF_INET Stream defaultProtocol
     let hints = defaultHints {
-                      addrFamily = AF_INET
-                    , addrSocketType = Stream
+                      addrFamily = AF_INET -- IPv4
+                    , addrSocketType = Stream -- TCP
                     }
+
+    -- Create this in order to automatically calculate 'SockAddr'
     addr <- head <$> getAddrInfo (Just hints) Nothing (Just "qotd")
     bind sock $ addrAddress addr
     return sock
@@ -32,13 +52,15 @@ forkServerForEach sock = do
     -- Create new forked process for one socket
     void $ forkFinally (server sock') (const $ gracefulClose sock' 5000)
 
+--
+-- | Actual server to receive request & send response
 server sock = do
-    recieved <- recv sock 1024
-    print recieved
+    recieved <- recv sock 1024  -- Receive 1024 bites from sock
     quote <- pickRandomQuote
     send sock $ quote `append` "\r\n"
 
 
+-- | Picking up random quote from file
 pickRandomQuote :: IO ByteString
 pickRandomQuote = do
     quotes <- lines <$> readFile "resources/quotes.txt"
