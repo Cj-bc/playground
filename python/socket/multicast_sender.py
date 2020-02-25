3cf3c1bdfa5a104b90be59a787aa72881220d22c
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-25 17:41:59 +0900

    Fix: py/socket/multicast_sender> ENABLE IP_MULTICAST_LOOP to get loop back

--- python/socket/multicast_sender.py
+++ python/socket/multicast_sender.py
@@ -6,7 +6,6 @@ port = 5002
 
 s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-s.setsockopt(IPPROTO_IP, IP_MULTICAST_LOOP, 0)
 s.setsockopt(IPPROTO_IP, IP_MULTICAST_IF, inet_aton("127.0.0.1"))
 
 
