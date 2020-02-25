a6427c705bed490f78d87b6d763e88655b617bfc
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-25 17:53:56 +0900

    Change: py/socket/multicast_reciever> enable SO_REUSEADDRESS

--- python/socket/multicast_reciever.py
+++ python/socket/multicast_reciever.py
@@ -3,6 +3,7 @@ from socket import *
 s = socket(AF_INET, SOCK_DGRAM)
 port = 5002
 
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # enable reusing the same address on the same time
 s.bind(('', port))
 try:
     while True:
