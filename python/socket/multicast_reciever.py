5cc59e7dd8540fb69415ed265666ed40f2040f24
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-25 17:57:30 +0900

    Fix: hs/socket/multicast_reciever> descriptions

--- python/socket/multicast_reciever.py
+++ python/socket/multicast_reciever.py
@@ -1,21 +1,27 @@
 from socket import *
 
-s = socket(AF_INET, SOCK_DGRAM)
+# マルチキャストアドレス
+# 詳しくは[multicast_sender.py](multicast_reciever.py) のドキュメント参考
 multicast_group = "239.255.0.1"
+
+
 multicast_if_addr = "0.0.0.1"
 port = 5002
 
+s = socket(AF_INET, SOCK_DGRAM) # Create socket with INET(IPv4), UDP
 s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # enable reusing the same address on the same time
 s.bind(('', port))
 
 # Create 'ip_mreq' structure (in c)
 mreq = inet_aton(multicast_group)+inet_aton(multicast_if_addr)
 
-s.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq) # Join multicast group
+# これによりMEMBERSHIPにjoinできる。
+# そうするとrecvで受け取れるようになる。
+s.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+
 try:
     while True:
-        msg = s.recv(48)
-        print(msg)
+        print(s.recv((48)))
 except KeyboardInterrupt:
     pass
 finally:
