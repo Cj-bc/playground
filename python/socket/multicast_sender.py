3cf23afc271ea2a1ff4a602b956fe319d0c9ca5e
Cj-bc <cj.bc-sd@outlook.jp>
2020-02-25 17:41:14 +0900

    Fix: py/socket/multicast_sender> sendto multicast_group

--- python/socket/multicast_sender.py
+++ python/socket/multicast_sender.py
@@ -1,5 +1,9 @@
 from socket import *
 
+multicast_group = "239.255.0.1"
+port = 5002
+
+
 s = socket(AF_INET, SOCK_DGRAM)
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 s.setsockopt(IPPROTO_IP, IP_MULTICAST_LOOP, 0)
@@ -7,8 +11,10 @@ s.setsockopt(IPPROTO_IP, IP_MULTICAST_IF, inet_aton("127.0.0.1"))
 
 
 try:
+    c=0
     while True:
-        s.sendto(b"abcdef", ("localhost", 5003))
+        s.sendto(b"hello message: " + str(c).encode(), (multicast_group, port))
+        c+=1
 except KeyboardInterrupt:
     pass
 finally:
