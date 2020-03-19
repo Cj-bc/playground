0e7c86a02448592cdd85655a1e755fd9c1f5cef5
Cj-bc <cj.bc-sd@outlook.jp>
2020-03-19 12:48:47 +0900

    Add: hs/hjq> Main

--- haskell/hjq/app/Main.hs
+++ haskell/hjq/app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
-
-import Lib
+import Data.Hjq
+import System.Environment
+import System.Exit
+import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+
+    case args of
+        (query:file:[]) -> do
+            json <- B.readFile file
+            printResult $ hjq json (T.pack query)
+
+        (query:[]) -> do
+            json <- B.getContents
+            printResult $ hjq json (T.pack query)
+
+        _ -> do putStrLn $ "Invalid arguments error.: " ++ show args
+                exitWith $ ExitFailure 1
+
+printResult :: Either T.Text B.ByteString -> IO ()
+printResult (Right s) = B.putStrLn s
+printResult (Left e) = do
+    TI.putStrLn e
+    exitWith $ ExitFailure 1
