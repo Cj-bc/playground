1396549dd00e8048bfc7e1052a53a5f82be1aaec
Cj-bc <cj.bc-sd@outlook.jp>
2021-01-31 10:55:24 +0900

    Fix: python/maze> きた道を再探索しないように変更
    以前の実装だと、そのマスの上下左右4方向に探索するため、きた道も遡って探索していた。
    きた道には帰りたくないが、どこから来たのかの情報がなかったため追加して帰らないようにした。

--- python/maze/solver.py
+++ python/maze/solver.py
@@ -19,22 +19,22 @@ class Solver:
         self.maze = m
 
     def solve(self):
-        ret = self.loop(self.maze.start)
+        ret = self.loop(self.maze.start, self.maze.start)
         if ret is None:
             print("見つかりませんでした")
         else:
             print(ret)
 
-    def loop(self, c) -> List[Coord]:
+    def loop(self, prev, c) -> List[Coord]:
         self.logger.debug(f"Looking around {c}...")
-        for n in [c.up(), c.down(), c.right(), c.left()]:
+        for n in filter(lambda a: a != prev, [c.up(), c.down(), c.right(), c.left()]):
             if n == self.maze.goal:
                 self.logger.debug(f"{n} is goal. Return")
                 return [n, c]
 
             if self.maze.isCellType(n, CellType.PATH):
                 self.logger.debug(f"Look for {n}...")
-                ret = self.loop(n)
+                ret = self.loop(c, n)
                 if ret != None:
                     self.logger.debug(f"Back...")
                     return ret.append(c)
