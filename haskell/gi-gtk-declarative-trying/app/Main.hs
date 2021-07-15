a639ca9975b4db89a81132b61aed9c5f17593fd6
Cj-bc <cj.bc-sd@outlook.jp>
2021-07-15 23:38:06 +0900

    Change: haskell/gi-gtk-declarative-trying> use Map with key 'UUID' to store all todo
    This is better way because it won't remove all todoes with the same name

--- haskell/gi-gtk-declarative-trying/app/Main.hs
+++ haskell/gi-gtk-declarative-trying/app/Main.hs
@@ -7,16 +7,23 @@ import           Control.Monad                  ( void )
 import           Data.Text                      ( Text
                                                 , pack
                                                 )
+import Data.UUID    (UUID)
+import qualified Data.UUID as UUID
+import Data.UUID.V4 (nextRandom)
 import           GI.Gtk                         ( Window(..)
                                                 , Box(..)
                                                 , Label(..)
                                                 , ListBox(..)
                                                 , ListBoxRow(..)
                                                 , Button(..)
+                                                , Entry(..)
+                                                , Separator(..)
                                                 )
 import           GI.Gtk.Declarative
 import           GI.Gtk.Declarative.App.Simple
 import qualified Data.Vector as V
+import qualified Data.Map as M
+import Control.Lens (ix, over)
 
 data TodoState = TODO | DONE deriving (Show, Eq)
 
@@ -24,55 +31,75 @@ data TodoItem = TodoItem { todoTitle :: Text
                          , todoState :: TodoState
                          , todoDescription :: Text
                          }
-type State = V.Vector TodoItem
 
-data Event = AddItem { _item_name :: Text
+appendNewItem :: State -> Text -> Text -> IO State
+appendNewItem s title desc = M.insert <$> nextRandom
+                                      <*> return (TodoItem title TODO desc)
+                                      <*> return s
+  
+type State = M.Map UUID TodoItem
+
+data Event = AddItem { _item_uuid :: UUID
+                     , _item_name :: Text
                      , _item_desc :: Text
                      }
-             | DoneTodo Text -- ^ use title to specify.
-             | RemoveTodo Text -- ^ use title to specify.
+             | DoneTodo UUID -- ^ use title to specify.
+             | RemoveTodo UUID -- ^ use title to specify.
              | AppClosed
 
 update' :: State -> Event -> Transition State Event
-update' s (AddItem title desc) = Transition (s V.++ [newItem]) (return Nothing)
+update' s (AddItem uuid title desc) = Transition (M.insert uuid newItem s) (return Nothing)
   where
     newItem = TodoItem title TODO desc
-update' s (DoneTodo title) = Transition newState (return Nothing)
+update' s (DoneTodo uuid) = Transition newState (return Nothing)
   where
-    newState = V.map (\i -> if todoTitle i == title
-                     then i { todoState = DONE }
-                     else i) s
-update' s (RemoveTodo title) = Transition newState ( return Nothing)
+    newState = over (ix uuid) (\i -> i { todoState = DONE }) s
+update' s (RemoveTodo uuid) = Transition newState ( return Nothing)
   where
-    newState = V.filter (\i -> (todoTitle i) /= title) s
+    newState = M.delete uuid s
 update' _ AppClosed = Exit
 
 
-todoWidget :: TodoItem -> Widget Event
-todoWidget (TodoItem title state desc)
+-- | 'Widget' for one 'TodoItem'
+todoWidget :: UUID -> TodoItem -> Widget Event
+todoWidget uuid (TodoItem title state desc)
   = container Box [] $ [ widget Label [#label := (pack . show $ state)]
                        , widget Label [#label := title]
                        , widget Label [#label := desc]
                        ] V.++ doneButton
   where
     doneButton | state == DONE = []
-               | otherwise = [widget Button [#label := "mark as done", on #clicked (DoneTodo title)]]
+               | otherwise = [widget Button [#label := "mark as done", on #clicked (DoneTodo uuid)]]
 
 -- | 'Widget' for List of toodes
 todoesWidget :: State -> Widget Event  
-todoesWidget s = container ListBox [] ( V.map (\i -> bin ListBoxRow [] $ todoWidget i) s) 
+todoesWidget = container ListBox [] . mapToVector . M.mapWithKey (\k i -> bin ListBoxRow [] $ todoWidget k i)
+  where
+    mapToVector :: M.Map k v -> V.Vector v
+    mapToVector = V.fromList . fmap snd . M.toList
+
+newItemWidget :: Widget Event
+newItemWidget = container Box [] [ widget Button [#label := "hoge"]
+                                 ,widget Entry [ #text := "title" ]
+                                 ,widget Entry [ #text := "description" ]
+                                 , widget Button [#label := "New item"
+                                                 , onM #clicked (const (AddItem <$> nextRandom
+                                                                         <*> return "New Item"
+                                                                         <*> return "Description"))]
+                                 ]
+                                 
 
 view' :: State -> AppView Window Event
 view' s =
   bin Window [#title := "Todo List"
              , on #deleteEvent (const (True, AppClosed))
              ] $ container ListBox [] [ bin ListBoxRow [] (todoesWidget s)
-                                      , bin ListBoxRow [] $ widget Button [#label := "New item", on #clicked (AddItem "New Item" "Description")]
+                                      , bin ListBoxRow [] newItemWidget
                                       ]
   
 -- | Making ToDo App
 main = void $ run App { view = view'
                       , update = update'
                       , inputs = []
-                      , initialState  = [TodoItem "Test" TODO "This is test entry"]
+                      , initialState  = M.fromList [(UUID.nil, TodoItem "Test" TODO "This is test entry" )]
                       }
