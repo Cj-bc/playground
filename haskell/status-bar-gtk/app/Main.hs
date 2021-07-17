f6dfb0e3b876911aa9e20bd96a5c1ee939d284d5
Cj-bc <cj.bc-sd@outlook.jp>
2021-07-18 00:08:49 +0900

    Change: haskell/status-bar-gtk> Split into some units

--- haskell/status-bar-gtk/app/Main.hs
+++ haskell/status-bar-gtk/app/Main.hs
@@ -7,6 +7,7 @@ module Main where
 
 import Control.Monad (void)
 import Data.Functor ((<&>))
+import qualified Data.Vector as V
 import qualified GI.Gtk as Gtk
 import GI.Gtk ( Box(..)
               , Window(..)
@@ -14,28 +15,35 @@ import GI.Gtk ( Box(..)
 import GI.Gtk.Declarative
 import GI.Gtk.Declarative.App.Simple
 import Control.Lens (makeLenses, (^.), over, each)
+import qualified Control.Lens as L
 import StatusBar.Type
+import StatusBar.Units.Pactl
 
 data Event = AppClose
 
-slider :: Percentage -> BoxChild Event
-slider p = BoxChild defaultBoxChildProperties $ widget Gtk.ProgressBar [#fraction := p]
+data State = State {_volumeSinks :: V.Vector VolumeSink -- ^ List of volume sinks
+                   }
+makeLenses ''State
 
-sliders :: State -> Widget Event
-sliders s = container Box [#orientation := Gtk.OrientationVertical
+
+slider :: VolumeSink -> BoxChild Event
+slider = BoxChild defaultBoxChildProperties . pactlVolume 
+
+volumeSliders :: State -> Widget Event
+volumeSliders s = container Box [#orientation := Gtk.OrientationVertical
                           , #spacing := 100
-                          ] $ [0.1, 0.3, 0.01] <&> slider
--- sliders s = container Box [] $ over each (\i -> i^.currentVolumePercentage) (s^.volumeSinks) <&> slider
+                          , classes ["volume"]
+                          ] $ s^.volumeSinks <&> slider
 
-update' _ AppClose = Exit
 
   
+update' _ AppClose = Exit
 
 main :: IO  ()
 main = do
   let app = App { view = bin Window [#name := "Status-bar-gtk"
                                     , on #deleteEvent (const (True, AppClose))
-                                    ] . sliders
+                                    ] . volumeSliders
                 , update = update'
                 , inputs = []
                 , initialState = State [(VolumeSink False 0.1), (VolumeSink False 0.8)]
--- haskell/status-bar-gtk/src/StatusBar/Type.hs
+++ haskell/status-bar-gtk/src/StatusBar/Type.hs
@@ -4,12 +4,3 @@ import Control.Lens (makeLenses)
 import qualified Data.Vector as V
 
 type Percentage = Double
-
-data VolumeSink = VolumeSink { _muted :: Bool
-                             , _currentVolumePercentage :: Percentage
-                             }
-makeLenses ''VolumeSink
-
-data State = State {_volumeSinks :: V.Vector VolumeSink -- ^ List of volume sinks
-                   }
-makeLenses ''State
