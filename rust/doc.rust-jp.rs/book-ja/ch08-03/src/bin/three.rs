1a950636f2f938221a09de937368a29bcb23e6ef
Cj-bc <cj.bc-sd@outlook.jp>
2022-03-01 18:17:39 +0900

    Add: rust/doc.rust-jp.rs> Implement list_entry and add_entry

--- rust/doc.rust-jp.rs/book-ja/ch08-03/src/bin/three.rs
+++ rust/doc.rust-jp.rs/book-ja/ch08-03/src/bin/three.rs
@@ -17,7 +17,16 @@ fn main() {
 }
 
 fn add_entry(db: &mut HashMap<String, Vec<String>>, name: &str, office: &str) {
+    let entry: &mut Vec<String> = db.entry(office.to_string()).or_default();
+    entry.push(String::from(name));
+    println!("{} was added to the office {}", name, office);
 }
 
 fn list_entry(db: &mut HashMap<String, Vec<String>>, office: &str) {
+    if let Some(names) = db.get(&String::from(office)) {
+	println!("list of names in office '{}'", &office);
+	for name in names.iter() {
+	    println!("- {}", name);
+	}
+    }
 }
