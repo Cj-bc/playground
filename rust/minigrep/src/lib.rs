ad6b6c4760dc1c22807c0de9e4389cbc44d99efa
Cj-bc <cj.bc-sd@outlook.jp>
2022-03-03 23:56:25 +0900

    Fix: rust/minigrep> use Box<dyn Error> ; idk why I need this, but it's requested by compiler

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -2,7 +2,7 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::error::Error;
 
-pub fn run(config: Config) -> Result<(), Box<Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(&config.filename)?;
 
     let mut content = String::new();
