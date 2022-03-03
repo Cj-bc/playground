af53504677eabcdf550486142b1e464345250178
Cj-bc <cj.bc-sd@outlook.jp>
2022-03-03 22:01:39 +0900

    Change: rust/minigrep> Create Config struct

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,16 +1,37 @@
 use std::env;
 use std::io::prelude::*;
 use std::fs::File;
+use std::process;
 
 fn main() {
     let args: Vec<String> =  env::args().collect();
-    let query: &String = &args[1];
-    let filename: &String = &args[2];
+    let cfg = Config::new(&args).unwrap_or_else(|err| {
+	println!("Problem parsing arguments: {}", err);
+	process::exit(1);
+    });
 
-    let mut f = File::open(filename).expect("Couldn't open file");
+    let mut f = File::open(&cfg.filename).expect("Couldn't open file");
 
     let mut content = String::new();
     f.read_to_string(&mut content).expect("Couldn't read from file");
 
-    println!("{}: {}", filename, content);
+    println!("{}: {}", cfg.filename, content);
+}
+
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+	if args.len() < 3 {
+	    return Err("Not enough arguments");
+	}
+
+	let query = args[1].clone();
+	let filename = args[2].clone();
+	Ok(Config { query, filename })
+    }
 }
