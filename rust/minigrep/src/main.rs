02323f1fcf3f006cfb4511c146bb585ea13c1bee
Cj-bc <cj.bc-sd@outlook.jp>
2022-03-03 20:54:35 +0900

    Change: rust/minigrep> Reading file content

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> =  env::args().collect();
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
+
+    let mut f = File::open(filename).expect("Couldn't open file");
+
+    let mut content = String::new();
+    f.read_to_string(&mut content).expect("Couldn't read from file");
+
+    println!("{}: {}", filename, content);
 }
