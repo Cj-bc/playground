14ee6c62582c6de0fd6f714b16e973062b02550b
Cj-bc <cj.bc-sd@outlook.jp>
2022-09-12 13:27:30 +0900

    Change: haskell/writeLogInAsync> unmaskした所で例外処理を行えば、その外側には影響しない [WIP]
    但しこれだと、本体がループしている際そのループから抜け出すことは出来無いので困る。

--- haskell/writeLogInAsync/app/Main.hs
+++ haskell/writeLogInAsync/app/Main.hs
@@ -1,17 +1,18 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Control.Concurrent
 import Control.Concurrent.Async
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad (void)
 import Control.Monad.Writer 
-import Control.Exception (mask, finally, catch)
+import Control.Exception (mask, finally, catch, SomeException)
 main = do
-  a <- async $ mask $ \unmask -> (do
+  a <- async $ mask $ \unmask -> do
         (_, logged) <- runWriterT $ do
           tell "Hello first log.\n"
-          liftIO . void . unmask $ threadDelay 100000
+          liftIO . void . unmask $ (threadDelay 1000000) `catch` (\(e :: SomeException) -> pure ())
           tell "Log after long process\n"
 
-        putStrLn logged) `catch` (return . const ())
+        putStrLn logged
 
   threadDelay 5000
   cancel a
