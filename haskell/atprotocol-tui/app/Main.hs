1e65e037ca46afd99673271060839daf8a3fe0f9
Cj-bc <cj.bc-sd@outlook.jp>
2022-10-22 14:26:20 +0900

    Add: haskell/atprotocol-tui> Straightforward implementation to access user data

--- haskell/atprotocol-tui/app/Main.hs
+++ haskell/atprotocol-tui/app/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
-
-import Lib
+import Network.HTTP.Simple
+import Network.HTTP.Client
 
 main :: IO ()
-main = someFunc
+main = do
+  let service = "http://localhost:2583/xrpc/com.atproto.repoDescribe?user=bob.test" { requestHeaders = [("Accept", "application/ld+json")]}
+  bob'sRepoRes <- responseBody <$> httpLBS service
+  print bob'sRepoRes
+  return ()
