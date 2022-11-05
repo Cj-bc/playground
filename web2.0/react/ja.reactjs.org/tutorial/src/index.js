b0078e3506499eda39175f1d5a48cb6b41df1db0
Cj-bc <cj.bc-sd@outlook.jp>
2022-11-06 00:05:52 +0900

    Change: web2.0/react/ja.reactjs.org/tutorial> calculate winner

--- web2.0/react/ja.reactjs.org/tutorial/src/index.js
+++ web2.0/react/ja.reactjs.org/tutorial/src/index.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 function Square(props) {
     return (
 	<button className="square" onClick={props.handleClick}>
@@ -33,7 +54,9 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.IsNextX ? 'X' : 'O');
+      const winner = calculateWinner(this.state.cells);
+      const status = winner ? ('Winner: ' + winner)
+	    : 'Next player: ' + (this.state.IsNextX ? 'X' : 'O');
 
     return (
       <div>
