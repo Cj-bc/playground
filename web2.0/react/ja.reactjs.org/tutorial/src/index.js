638c133bc7ad3578d4a611845ee0b01a338d8eb0
Cj-bc <cj.bc-sd@outlook.jp>
2022-11-05 23:44:14 +0900

    Add: web2.0/react/ja.reactjs.org/tutorial> Tic-tac-Toe game [WIP]

---
+++ web2.0/react/ja.reactjs.org/tutorial/src/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+
+class Square extends React.Component {
+  render() {
+    return (
+	<button className="square" onClick={() => this.props.handleClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cells: Array(9).fill(null)
+    };
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.cells[i]} handleClick={() => this.handleClick(i)} />;
+  }
+
+  handleClick(index) {
+      console.log("handle click called!");
+      const cells  = this.state.cells.slice();
+      cells[index] = 'X';
+      this.setState({cells: cells});
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
