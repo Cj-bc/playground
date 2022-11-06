6270bb57658e3969070f6b2b74c335136392a54e
Cj-bc <cj.bc-sd@outlook.jp>
2022-11-06 09:49:44 +0900

    fix: indentation

--- web2.0/react/ja.reactjs.org/tutorial/src/index.js
+++ web2.0/react/ja.reactjs.org/tutorial/src/index.js
@@ -85,19 +85,19 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    render() {
+	return (
+	    <div className="game">
+		<div className="game-board">
+		    <Board />
+		</div>
+		<div className="game-info">
+		    <div>{/* status */}</div>
+		    <ol>{/* TODO */}</ol>
+		</div>
+	    </div>
+	);
+    }
 }
 
 // ========================================
