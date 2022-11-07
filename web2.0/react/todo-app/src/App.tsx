d280966c1b338af4ff1552c97fa5e5ce2fbf3da2
Cj-bc <cj.bc-sd@outlook.jp>
2022-11-08 00:13:34 +0900

    Add: web2.0/react/todo-app> Predefined todo tasks, toggle TODO status

--- web2.0/react/todo-app/src/App.tsx
+++ web2.0/react/todo-app/src/App.tsx
@@ -2,25 +2,64 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface TodoItem {
+    id: number,
+    name: string,
+    isDone: boolean,
+}
+
+
+function TodoItem(props: {entry: TodoItem, toggle: () => void}) {
+    const entry = props.entry;
+    return (<div className="Todo-Item">
+		<button className="Todo-done" onClick={props.toggle}> {entry.isDone ? "DONE" : "TODO"} </button>
+		<div className="Todo-Name"> {entry.name} </div>
+
+	    </div>)
+}
+
+function TodoList(props: TodoItem[]) {
+
+}
+
+class App extends React.Component<{}, {todoes: TodoItem[]}> {
+    constructor(props: any) {
+	super(props);
+
+	const todoes: TodoItem[] = [{id: 0, name: "Example", isDone: false },
+		     {id: 1, name: "2nd", isDone: false }
+		    ]
+ 
+	this.state = {
+	    todoes: todoes
+	};
+    }
+    
+    render() {
+	const todoes = this.state.todoes;
+	const items = todoes.map((item, idx) => {
+	    return (<li>
+			<TodoItem entry={item} toggle={() => this.toggleStateAt(item.id)} />
+		    </li>)})
+	
+	return (
+	    <div className="App">
+		<ol>
+		    {items}
+		</ol>
+	    </div>
+	);
+    }
+
+    toggleStateAt(id: number) {
+	const todoes = this.state.todoes;
+
+	this.setState({todoes: todoes.map((item) =>
+	    (item.id === id) ? {...item, isDone: !item.isDone} : item
+	)
+		      });
+	
+    }
 }
 
 export default App;
