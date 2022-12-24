99f042b2fd04aa74bbc32577c5fbb99e9b441ce0
Cj-bc <cj.bc-sd@outlook.jp>
2022-12-25 02:16:38 +0900

    Add: go/go.dev/tutorial/web-service-gin> '/album/:id' GET endpoint

--- go/go.dev/tutorial/web-service-gin/main.go
+++ go/go.dev/tutorial/web-service-gin/main.go
@@ -38,9 +38,28 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID (c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
