ec2d6102c196e215ee46bfbaa35aa1e748e5dc07
Cj-bc <cj.bc-sd@outlook.jp>
2022-12-24 23:19:51 +0900

    Change: go/go.dev/tutorial/greetings> "Return and handle an error - The Go Programming Language"
    https://go.dev/doc/tutorial/handle-errors

--- go/go.dev/tutorial/greetings/greetings.go
+++ go/go.dev/tutorial/greetings/greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
-import "fmt"
+import ("fmt"
+	"errors")
 
-func Hello(name string) string {
-     // Return a greeting that embeds the name in a message.
-     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-     return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
