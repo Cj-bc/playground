8d2cd259128d4a48d3e990f5f8100babc6ad2b47
Cj-bc <cj.bc-sd@outlook.jp>
2022-12-26 23:43:08 +0900

    Add: go/bubbletea/todo-list> Just trying to write tiny todo app sample by myself
    Although tutorial had similar project, I wanted to do this by myself.

---
+++ go/bubbletea/todo-list/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// Task Model {{{
+type task struct {
+	title string
+	isDone bool
+	description string
+}
+
+type updateMsg func(task) task
+type toggleDoneMsg struct{}
+
+func (t task) Init() tea.Cmd {
+	return nil
+}
+
+func (t task) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg.(type) {
+		//case updateMsg:
+		//	//return func(task) task(msg)(t), nil
+	//	task(msg).title
+	case toggleDoneMsg:
+		t.isDone = !t.isDone
+	}
+
+	return t, nil
+}
+
+func (t task) View() string {
+	return t.title
+}
+// }}}
+
+// Model declarations {{{
+type model struct {
+	tasks []task
+	cursor int
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "j":
+			if m.cursor < len(m.tasks)-1 {
+				m.cursor++
+			}
+		case "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case " ":
+			updatedTask, cmd := m.tasks[m.cursor].Update(toggleDoneMsg{})
+			if task, ok := updatedTask.(task); ok {
+				m.tasks[m.cursor] = task
+			}
+			
+			return m, cmd
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := ""
+	for i, item := range m.tasks {
+		cursor := " "
+		if i == m.cursor { cursor = ">"; }
+
+		isDone := " "
+		if item.isDone { isDone = "x"; }
+		
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, isDone, item.title)
+	}
+	return s
+}
+// }}}
+
+func main() {
+	testTasks := []task{
+		task{title: "FooBar"},
+		task{title: "English"},
+		task{title: "This is absolutely nonsence btw"},
+	}
+
+	tea.NewProgram(model{tasks: testTasks, cursor: 0}).Run()
+}
