c6a01bd6fd44d92a1770e88cd8f10b7019e8fed7
Cj-bc <cj.bc-sd@outlook.jp>
2022-12-27 16:02:34 +0900

    Change: go/bubbletea/todo-list> Use custom itemDelegate for list

--- go/bubbletea/todo-list/main.go
+++ go/bubbletea/todo-list/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/bubbles/list"
@@ -34,16 +35,31 @@ func (t task) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return t, nil
 }
 
-func (t task) View() string {
-	isDone := " "
-
-
-	return fmt.Sprintf("[%s] %s", isDone, t.title)
-}
-
+func (t task) View() string { return t.title }
 func (t task) Title() string { return t.title }
 func (t task) Description() string { return t.description }
 func (t task) FilterValue() string { return t.title }
+
+type taskDelegate struct {}
+
+func (delgate taskDelegate) Render(w io.Writer, m list.Model, index int, item list.Item) {
+	var task_ task
+	
+	if item, ok := item.(task); ok {
+		task_ = item
+	} else {
+		return
+	}
+	if m.Width() <= 0 { return }
+
+	fmt.Fprintf(w, "%s\n%s", task_.title, task_.description)
+}
+
+func (delegate taskDelegate) Height() int { return 5 }
+func (delegate taskDelegate) Spacing() int { return 1 }
+func (delegate taskDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
+	return nil
+}
 
 // }}}
 
@@ -70,10 +86,10 @@ func (m model) View() string {
 
 func main() {
 	testTasks := list.New([]list.Item {
-		task{title: "FooBar"},
-		task{title: "English"},
-		task{title: "This is absolutely nonsence btw"},
-	}, list.NewDefaultDelegate(), 100, 50)
+		task{title: "FooBar", description: "This is dummy description"},
+			task{title: "English", description: "English is a language"},
+			task{title: "This is absolutely nonsence btw"},
+	}, new(taskDelegate), 100, 50)
 
 	tea.NewProgram(model{tasks: testTasks, cursor: 0}).Run()
 }
