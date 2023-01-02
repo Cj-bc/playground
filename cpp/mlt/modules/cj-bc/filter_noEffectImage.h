e8b70394ddebb559b641f4b882488d1e8a0ed6ad
Cj-bc <cj.bc-sd@outlook.jp>
2023-01-02 17:37:53 +0900

    Add: cpp/mlt/noEffectImage> Idk if this is needed, but I wrote this as factory.c couldn't find some function in filter_noEffectImage.cpp

---
+++ cpp/mlt/modules/cj-bc/filter_noEffectImage.h
@@ -0,0 +1,5 @@
+#include <framework/mlt.h>
+#include <framework/mlt_filter.h>
+#include <framework/mlt_frame.h>
+
+mlt_filter filter_noEffectImage_init(mlt_profile profile, mlt_service_type type, const char *id, char *arg);
