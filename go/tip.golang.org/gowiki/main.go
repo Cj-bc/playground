af38e5de022988ff3752a06d4c354ddfa5780d24
Cj-bc <cj.bc-sd@outlook.jp>
2023-01-21 23:25:43 +0900

    go/gowiki> Editing Pages

--- go/tip.golang.org/gowiki/main.go
+++ go/tip.golang.org/gowiki/main.go
@@ -32,8 +32,22 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", page.Title, page.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>%s</h1>"+
+		 "<form action\"/save/%s\" method=\"POST\">" +
+		 "<textarea name=\"body\">%s</textarea><br />" +
+		 "<input type=\"submit\" value=\"Save\">" +
+		 "</form>", p.Title, p.Title, p.Body)
+}
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
