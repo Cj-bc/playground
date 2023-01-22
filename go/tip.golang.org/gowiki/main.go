f70a42972a2117efdd4bff90a784e023cb457b0b
Cj-bc <cj.bc-sd@outlook.jp>
2023-01-22 09:12:41 +0900

    go/gowiki> Saving Pages

--- go/tip.golang.org/gowiki/main.go
+++ go/tip.golang.org/gowiki/main.go
@@ -51,8 +51,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit.html", page)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
