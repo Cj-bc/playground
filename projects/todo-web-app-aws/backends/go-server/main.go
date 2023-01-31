74d35392e55ce74402398404ebf347277f9dc5c9
Cj-bc <cj.bc-sd@outlook.jp>
2023-01-31 23:32:07 +0900

    Add: prj/todo-web-app-aws/go-server> Initialize ent

--- projects/todo-web-app-aws/backends/go-server/main.go
+++ projects/todo-web-app-aws/backends/go-server/main.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go-server/ent"
+	"go-server/ent/todo"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	// Preparing client
+	client, err := ent.Open("sqlite3", "./todoes.sqlite3")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background); err != nil {
+		log.Fatalf ("failed creating schema resources: %v", err)
+	}
+	// Preparing client done
+
+
 }
