31408a0a692234d05ddd9a08fa0c5033e61d2b20
Cj-bc <cj.bc-sd@outlook.jp>
2023-02-27 14:05:02 +0900

    c/mlt> writing simple app based on tutorial and melt source code
    Note that I am not sure how safe this program is. I'm writing this
    to understand how mlt library works in C, not to build something upon it.

---
+++ c/mlt/simple-media-player.c
@@ -0,0 +1,71 @@
+/*
+  simple-media-player -- my practice program to use MLT library
+
+  Copyright 2023 Cj-bc a.k.a Cj.bc_sd
+
+  Also note that I borrowed some of parts from
+  - https://github.com/mltframework/mlt/blob/769cf9dd0aec54221f8fb8cbc4b4f50630136578/src/melt/melt.c#L983-L988
+  - https://www.mltframework.org/docs/framework/
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version. 
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details. 
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
+
+  This program is based on
+  https://www.mltframework.org/docs/framework/,
+  with modification to make it work with current mlt library and
+  graceful exit from melt code above.
+*/
+#include <stdio.h>
+#include <framework/mlt.h>
+#include <signal.h>
+
+// In order to gracefully close consumer on signal, I have to make it static
+static mlt_consumer consumer;
+
+void signal_handler(int signum) {
+  if (consumer) {
+    mlt_properties prop = MLT_CONSUMER_PROPERTIES (consumer);
+    mlt_properties_set_int(prop, "done", 1);
+  }
+}
+
+int main (int argc, char *argv[]) {
+  if (argc <= 1) {
+    fprintf(stdout, "simple-media-player: plays movies in serial form\nsimple-media-player [MOVIE_PATH ...]\n");
+    return 0;
+  }
+  mlt_repository repo = mlt_factory_init(NULL);
+
+  if (!repo) {
+    fprintf(stderr, "Failed to initialize factory");
+    return -1;
+  }
+
+  mlt_profile profile = mlt_profile_init(NULL);
+
+  mlt_playlist playlist = mlt_playlist_init();
+  
+  for (int i = 1; i < argc; i++) {
+    mlt_producer prod = mlt_factory_producer(profile, NULL, argv[i]);
+
+    mlt_playlist_append(playlist, prod);
+    mlt_producer_close(prod);
+  }
+
+  for (int i = 0; i < mlt_playlist_count(playlist); i++) {
+    mlt_playlist_clip_info info;
+    mlt_playlist_get_clip_info(playlist, &info, i);
+    fprintf(stdout, "playlist[%i]: %s\n", i, info.resource);
+  }
+}
