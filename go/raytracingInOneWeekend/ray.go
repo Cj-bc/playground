90193e10082a199dd2eabf958718221445018ddc
Cj-bc <cj.bc-sd@outlook.jp>
2023-03-25 17:51:34 +0900

    fix: go/raytracingInOneWeekend/ray> Remove Ray function to avoid name conflict

--- go/raytracingInOneWeekend/ray.go
+++ go/raytracingInOneWeekend/ray.go
@@ -6,10 +6,6 @@ type Ray struct {
 	Direction vec3.Vec3
 }
 
-func Ray(orig Vec3, dir Vec3) Ray {
-	return Ray{orig, dir}
-}
-
 func (r Ray) At(t float64) vec3.Vec3 {
 	return r.Origin.Add(r.Direction.MulScalar(t))
 }
