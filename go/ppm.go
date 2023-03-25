35ee4aabfeb8ec16e8e9175ff50f6f432ae9382a
Cj-bc <cj.bc-sd@outlook.jp>
2023-03-25 10:02:23 +0900

    Change: go/ppm> support width/height changes

--- go/ppm.go
+++ go/ppm.go
@@ -5,14 +5,16 @@ import "os"
 import "fmt"
 
 func main() {
-	width, height := 256, 256
+	width, height := 256.0, 256.0
 
 	file := os.Stdout
 
-	file.WriteString(fmt.Sprintf("P3\n%d %d\n255\n", width, height))
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			file.WriteString(fmt.Sprintf("%d %d %d ", w, h, 0))
+	file.WriteString(fmt.Sprintf("P3\n%d %d\n255\n", int(width), int(height)))
+	for h := 0.0; h < height; h++ {
+		for w := 0.0; w < width; w++ {
+			r := w / width * 255
+			g := h / height * 255
+			file.WriteString(fmt.Sprintf("%d %d %d ", int(r), int(g), 0))
 		}
 		file.WriteString("\n")
 	}
