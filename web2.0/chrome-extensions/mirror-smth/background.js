b74e1fe4bd5c9a4834ae0d5693a7e796d199f2d8
Cj-bc <cj.bc-sd@outlook.jp>
2023-03-29 12:32:00 +0900

    Change: chrome-extensions/mirror-smth> Enable it only on youtube
    https://developer.chrome.com/docs/extensions/reference/action/#emulating-pageactions-with-declarativecontent

--- web2.0/chrome-extensions/mirror-smth/background.js
+++ web2.0/chrome-extensions/mirror-smth/background.js
@@ -1,3 +1,15 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.disable();
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+	let enableActionRule = {
+	    conditions: [new chrome.declarativeContent.PageStateMatcher({pageUrl: {hostSuffix: "youtube.com"}})],
+	    actions: [new chrome.declarativeContent.ShowAction()]
+	}
+	chrome.declarativeContent.onPageChanged.addRules([enableActionRule]);
+    });
+});
+
 chrome.action.onClicked.addListener(async (tab) => {
     chrome.windows.create({url: 'mirrored.html'});
 });
