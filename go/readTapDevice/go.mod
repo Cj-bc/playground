d047b0e97f4ab7dbf7017654c49b1323b1ae189b
Cj-bc <cj.bc-sd@outlook.jp>
2023-05-02 12:01:56 +0900

    Add: go/readTapDevice> tiny code to read from tap device

---
+++ go/readTapDevice/go.mod
@@ -0,0 +1,5 @@
+module github.com/Cj-bc/plg/readTapDevice
+
+go 1.20
+
+require golang.org/x/sys v0.7.0
---
+++ go/readTapDevice/readTapDevice.go
@@ -0,0 +1,54 @@
+package main
+import (
+	"golang.org/x/sys/unix"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	// https://mirrors.edge.kernel.org/pub/linux/kernel/people/marcelo/linux-2.4/Documentation/networking/tuntap.txt
+	fd, err := unix.Open("/dev/net/tun", unix.O_RDWR, 0)
+	defer unix.Close(fd)
+	if err != nil {
+		fmt.Println("unix.Open: ", err.Error())
+		os.Exit(1)
+	}
+
+	ifr, err := unix.NewIfreq("tap0")
+	if err != nil {
+		fmt.Println("unix.NewIfreq: ", err.Error())
+		os.Exit(1)
+	}
+	ifr.SetUint16(unix.IFF_TAP)
+	if err := unix.IoctlIfreq(fd, unix.TUNSETIFF, ifr); err != nil {
+		fmt.Println("unix.IoctlIfreq: ", err.Error())
+		os.Exit(1)
+	}
+
+	listening := make(chan int, 1)
+	go func() {
+		received := make([]byte, 10000)
+		if n, err := unix.Read(fd, received); err != nil {
+			fmt.Println("unix.Read: ", err.Error())
+			listening <- 1
+		} else {
+			received = received[:n]
+		}
+		fmt.Println(received)
+
+		listening <- 0
+	}()
+
+	timeout := time.After(20 * time.Second)
+
+OuterLoop:
+	for {
+		select {
+		case <-timeout:
+			break OuterLoop
+		case <-listening:
+			break OuterLoop
+		}
+	}
+}
