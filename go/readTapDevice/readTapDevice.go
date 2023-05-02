10ba9a787dc8de2d23f89210d1c8e7f11c7c76f2
Cj-bc <cj.bc-sd@outlook.jp>
2023-05-02 16:53:27 +0900

    Change: go/readTapDevice> Implement "ip link set DEV up" functionality

--- go/readTapDevice/readTapDevice.go
+++ go/readTapDevice/readTapDevice.go
@@ -28,6 +28,36 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Equivalent to `ip link set <dev> up'
+	// According to do_chflags in iplink.c
+	// https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/ip/iplink.c#n1203
+
+	// We have to create socket, then do `ioctl' against it
+	// TODO: Original code is in get_ctl_fd in iplink.c. Understand how it works.
+	fdsock, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
+	if err != nil {
+		fmt.Println("unix.Socket: ", err.Error())
+		os.Exit(1)
+	}
+
+	// Retrive current active flag words.
+	ifr, err = unix.NewIfreq(DEVICENAME)
+	if err != nil {
+		fmt.Println("unix.NewIfreq for SIOCGIFFLAGS: ", err.Error())
+		os.Exit(1)
+	}
+	if err := unix.IoctlIfreq(fdsock, unix.SIOCGIFFLAGS, ifr); err != nil {
+		fmt.Println("unix.IoctlIfreq for SIOCGIFFLAGS: ", err.Error())
+		os.Exit(1)
+	}
+
+	// Set IFF_UP leaving other stuff unchanged
+	ifr.SetUint16((ifr.Uint16() &^ unix.IFF_UP) | unix.IFF_UP)
+	if err := unix.IoctlIfreq(fdsock, unix.SIOCSIFFLAGS, ifr); err != nil {
+		fmt.Println("unix.IoctlIfreq for SIOCSIFFLAGS: ", err.Error())
+		os.Exit(1)
+	}
+
 	listening := make(chan int, 1)
 	go func() {
 		received := make([]byte, 10000)
