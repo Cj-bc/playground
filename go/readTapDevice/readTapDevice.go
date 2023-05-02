2deb5d85f0a88a43fd46a24c77f49cfef32a7f2a
Cj-bc <cj.bc-sd@outlook.jp>
2023-05-02 17:24:53 +0900

    refactor: go/readTapDevice> Split code into functions for readability

--- go/readTapDevice/readTapDevice.go
+++ go/readTapDevice/readTapDevice.go
@@ -9,52 +9,15 @@ import (
 const DEVICENAME = "tap1"
 
 func main() {
-	// https://mirrors.edge.kernel.org/pub/linux/kernel/people/marcelo/linux-2.4/Documentation/networking/tuntap.txt
-	fd, err := unix.Open("/dev/net/tun", unix.O_RDWR, 0)
+	fd, err := makeTap(DEVICENAME)
 	defer unix.Close(fd)
 	if err != nil {
-		fmt.Println("unix.Open: ", err.Error())
+		fmt.Println("makeTap: ", err.Error())
 		os.Exit(1)
 	}
 
-	ifr, err := unix.NewIfreq(DEVICENAME)
-	if err != nil {
-		fmt.Println("unix.NewIfreq: ", err.Error())
-		os.Exit(1)
-	}
-	ifr.SetUint16(unix.IFF_TAP)
-	if err := unix.IoctlIfreq(fd, unix.TUNSETIFF, ifr); err != nil {
-		fmt.Println("unix.IoctlIfreq: ", err.Error())
-		os.Exit(1)
-	}
-
-	// Equivalent to `ip link set <dev> up'
-	// According to do_chflags in iplink.c
-	// https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/ip/iplink.c#n1203
-
-	// We have to create socket, then do `ioctl' against it
-	// TODO: Original code is in get_ctl_fd in iplink.c. Understand how it works.
-	fdsock, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
-	if err != nil {
-		fmt.Println("unix.Socket: ", err.Error())
-		os.Exit(1)
-	}
-
-	// Retrive current active flag words.
-	ifr, err = unix.NewIfreq(DEVICENAME)
-	if err != nil {
-		fmt.Println("unix.NewIfreq for SIOCGIFFLAGS: ", err.Error())
-		os.Exit(1)
-	}
-	if err := unix.IoctlIfreq(fdsock, unix.SIOCGIFFLAGS, ifr); err != nil {
-		fmt.Println("unix.IoctlIfreq for SIOCGIFFLAGS: ", err.Error())
-		os.Exit(1)
-	}
-
-	// Set IFF_UP leaving other stuff unchanged
-	ifr.SetUint16((ifr.Uint16() &^ unix.IFF_UP) | unix.IFF_UP)
-	if err := unix.IoctlIfreq(fdsock, unix.SIOCSIFFLAGS, ifr); err != nil {
-		fmt.Println("unix.IoctlIfreq for SIOCSIFFLAGS: ", err.Error())
+	if err := linkUp(DEVICENAME); err != nil {
+		fmt.Println("linkUp: ", err.Error())
 		os.Exit(1)
 	}
 
@@ -83,4 +46,53 @@ OuterLoop:
 			break OuterLoop
 		}
 	}
+}
+
+func makeTap(dev string) (int, error) {
+	// https://mirrors.edge.kernel.org/pub/linux/kernel/people/marcelo/linux-2.4/Documentation/networking/tuntap.txt
+	fd, err := unix.Open("/dev/net/tun", unix.O_RDWR, 0)
+	if err != nil {
+		return -1, fmt.Errorf("unix.Open: %w", err)
+	}
+
+	// Setup TAP device
+	ifr, err := unix.NewIfreq(dev)
+	if err != nil {
+		return -1, fmt.Errorf("unix.NewIfreq: %w", err)
+	}
+	ifr.SetUint16(unix.IFF_TAP)
+	if err := unix.IoctlIfreq(fd, unix.TUNSETIFF, ifr); err != nil {
+		return -1, fmt.Errorf("unix.IoctlIfreq: %w", err)
+	}
+
+	return fd, nil
+}
+
+// Equivalent to `ip link set <dev> up'
+// According to do_chflags in iplink.c
+// https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/ip/iplink.c#n1203
+func linkUp(dev string) error {
+
+	// We have to create socket, then do `ioctl' against it
+	// TODO: Original code is in get_ctl_fd in iplink.c. Understand how it works.
+	fdsock, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
+	if err != nil {
+		return fmt.Errorf("unix.Socket: %w", err)
+	}
+
+	// Retrive current active flag words.
+	ifr, err := unix.NewIfreq(dev)
+	if err != nil {
+		return fmt.Errorf("unix.NewIfreq for SIOCGIFFLAGS: %w", err)
+	}
+	if err := unix.IoctlIfreq(fdsock, unix.SIOCGIFFLAGS, ifr); err != nil {
+		return fmt.Errorf("unix.IoctlIfreq for SIOCGIFFLAGS: %w", err)
+	}
+
+	// Set IFF_UP leaving other stuff unchanged
+	ifr.SetUint16((ifr.Uint16() &^ unix.IFF_UP) | unix.IFF_UP)
+	if err := unix.IoctlIfreq(fdsock, unix.SIOCSIFFLAGS, ifr); err != nil {
+		return fmt.Errorf("unix.IoctlIfreq for SIOCSIFFLAGS: %w", err)
+	}
+	return nil
 }
