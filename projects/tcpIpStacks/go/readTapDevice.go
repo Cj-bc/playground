256a3b493f2857eb524215e540a2aae65283a952
Cj-bc <cj.bc-sd@outlook.jp>
2023-05-06 22:56:15 +0900

    Change: projects/tcpIpStacks/go> separate Tap related codes

--- projects/tcpIpStacks/go/readTapDevice.go
+++ projects/tcpIpStacks/go/readTapDevice.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"os"
 	"time"
+	"github.com/Cj-bc/plg/tcpIpStacks/tap"
 )
 
 const DEVICENAME = "tap1"
 
 func main() {
-	fd, err := makeTap(DEVICENAME)
-	defer unix.Close(fd)
+	tap, err := tap.New(DEVICENAME)
+	defer unix.Close(tap.Fd)
 	if err != nil {
 		fmt.Println("makeTap: ", err.Error())
 		os.Exit(1)
 	}
 
-	if err := linkUp(DEVICENAME); err != nil {
+	if err := tap.Up(); err != nil {
 		fmt.Println("linkUp: ", err.Error())
 		os.Exit(1)
 	}
@@ -25,7 +26,7 @@ func main() {
 	listening := make(chan int, 1)
 	go func() {
 		received := make([]byte, 10000)
-		if n, err := unix.Read(fd, received); err != nil {
+		if n, err := unix.Read(tap.Fd, received); err != nil {
 			fmt.Println("unix.Read: ", err.Error())
 			listening <- 1
 		} else {
@@ -47,55 +48,4 @@ OuterLoop:
 			break OuterLoop
 		}
 	}
-}
-
-func makeTap(dev string) (int, error) {
-	// https://mirrors.edge.kernel.org/pub/linux/kernel/people/marcelo/linux-2.4/Documentation/networking/tuntap.txt
-	fd, err := unix.Open("/dev/net/tun", unix.O_RDWR, 0)
-	if err != nil {
-		return -1, fmt.Errorf("unix.Open: %w", err)
-	}
-
-	// Setup TAP device
-	ifr, err := unix.NewIfreq(dev)
-	if err != nil {
-		return -1, fmt.Errorf("unix.NewIfreq: %w", err)
-	}
-	ifr.SetUint16(unix.IFF_TAP)
-	if err := unix.IoctlIfreq(fd, unix.TUNSETIFF, ifr); err != nil {
-		return -1, fmt.Errorf("unix.IoctlIfreq: %w", err)
-	}
-
-	return fd, nil
-}
-
-// Equivalent to `ip link set <dev> up'
-// According to do_chflags in iplink.c
-// https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/ip/iplink.c#n1203
-//
-// Make sure device `dev' is available before call this
-func linkUp(dev string) error {
-
-	// We have to create socket, then do `ioctl' against it
-	// TODO: Original code is in get_ctl_fd in iplink.c. Understand how it works.
-	fdsock, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
-	if err != nil {
-		return fmt.Errorf("unix.Socket: %w", err)
-	}
-
-	// Retrive current active flag words.
-	ifr, err := unix.NewIfreq(dev)
-	if err != nil {
-		return fmt.Errorf("unix.NewIfreq for SIOCGIFFLAGS: %w", err)
-	}
-	if err := unix.IoctlIfreq(fdsock, unix.SIOCGIFFLAGS, ifr); err != nil {
-		return fmt.Errorf("unix.IoctlIfreq for SIOCGIFFLAGS: %w", err)
-	}
-
-	// Set IFF_UP leaving other stuff unchanged
-	ifr.SetUint16((ifr.Uint16() &^ unix.IFF_UP) | unix.IFF_UP)
-	if err := unix.IoctlIfreq(fdsock, unix.SIOCSIFFLAGS, ifr); err != nil {
-		return fmt.Errorf("unix.IoctlIfreq for SIOCSIFFLAGS: %w", err)
-	}
-	return nil
 }
---
+++ projects/tcpIpStacks/go/tap/tap.go
@@ -0,0 +1,62 @@
+package tap
+import (
+	"golang.org/x/sys/unix"
+	"fmt"
+)
+
+type Tap struct {
+	Fd int
+	Name string
+}
+
+func New(name string) (Tap, error) {
+	// https://mirrors.edge.kernel.org/pub/linux/kernel/people/marcelo/linux-2.4/Documentation/networking/tuntap.txt
+	fd, err := unix.Open("/dev/net/tun", unix.O_RDWR, 0)
+	if err != nil {
+		return Tap{}, fmt.Errorf("failed to Open /dev/net/tun file with O_RDWR: %w", err)
+	}
+
+	// Setup TAP device
+	ifr, err := unix.NewIfreq(name)
+	if err != nil {
+		return Tap{}, fmt.Errorf("Failed to initialize ifreq for TUNSETIFF: %w", err)
+	}
+	ifr.SetUint16(unix.IFF_TAP)
+	if err := unix.IoctlIfreq(fd, unix.TUNSETIFF, ifr); err != nil {
+		return Tap{}, fmt.Errorf("Failed to call ioctl for TUNSETIFF with IFF_TAP: %w", err)
+	}
+	return Tap{fd, name}, nil
+}
+
+
+// Equivalent to `ip link set <dev> up'
+// According to do_chflags in iplink.c
+// https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/tree/ip/iplink.c#n1203
+//
+// Make sure device `dev' is available before call this
+func (tap *Tap) Up() error {
+
+	// We have to create socket, then do `ioctl' against it
+	// TODO: Original code is in get_ctl_fd in iplink.c. Understand how it works.
+	fdsock, err := unix.Socket(unix.AF_INET, unix.SOCK_DGRAM, 0)
+	if err != nil {
+		return fmt.Errorf("Failed to create socket with AF_INET, SOCK_DGRAM: %w", err)
+	}
+
+	// Retrive current active flag words.
+	ifr, err := unix.NewIfreq(tap.Name)
+	if err != nil {
+		return fmt.Errorf("Failed to create Ifreq for SIOCGIFFLAGS: %w", err)
+	}
+	if err := unix.IoctlIfreq(fdsock, unix.SIOCGIFFLAGS, ifr); err != nil {
+		return fmt.Errorf("Failed to call ioctl for SIOCGIFFLAGS: %w", err)
+	}
+
+	// Set IFF_UP leaving other stuff unchanged
+	ifr.SetUint16((ifr.Uint16() &^ unix.IFF_UP) | unix.IFF_UP)
+	if err := unix.IoctlIfreq(fdsock, unix.SIOCSIFFLAGS, ifr); err != nil {
+		return fmt.Errorf("Failed to call ioctl for SIOCSIFFLAGS: %w", err)
+	}
+
+	return nil
+}
