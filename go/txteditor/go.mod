4483f3e850afdfc3405d8c2822e77b681a3b21dd
Cj-bc <cj.bc-sd@outlook.jp>
2023-08-24 18:03:44 +0900

    Add: go/txteditor> writing tiny text editor to test implementation of buffers

---
+++ go/txteditor/go.mod
@@ -0,0 +1,17 @@
+module github.com/Cj-bc/txteditor
+
+go 1.21.0
+
+require (
+	github.com/muesli/termenv v0.15.2
+	golang.org/x/term v0.11.0
+)
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+)
---
+++ go/txteditor/main.go
@@ -0,0 +1,51 @@
+package main
+import (
+	"log"
+	"os"
+	"github.com/muesli/termenv"
+	"golang.org/x/term"
+	"fmt"
+	"unicode/utf8"
+)
+
+type Record struct {
+	startIdx int
+	length int
+}
+
+type PieceTable struct {
+	origin []string
+	addition []string
+	records []Record
+}
+
+func main() {
+	logger := log.Default()
+	currentTerm := termenv.DefaultOutput()
+	
+	data, err := os.ReadFile("/etc/profile")
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	currentTerm.AltScreen()
+	defer currentTerm.ExitAltScreen()
+	currentTerm.Write(data)
+
+	connectedFd := int(currentTerm.TTY().Fd())
+	initialEnv, err := term.MakeRaw(connectedFd)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer term.Restore(connectedFd, initialEnv)
+
+	var tmp []byte
+	for i := 0; i < 10; i++ {
+		
+		fmt.Fscan(currentTerm.TTY(), &tmp)
+		rune, _ := utf8.DecodeRune(tmp)
+		if rune == 'q' {
+			break
+		}
+	}
+}
