6402c519aec53b17a31a324cde3b3c1a8835ab05
Cj-bc <cj.bc-sd@outlook.jp>
2023-08-25 17:25:45 +0900

    Change: go/txteditor> Split files

--- go/txteditor/main.go
+++ go/txteditor/main.go
@@ -1,58 +1,10 @@
 package main
 import (
 	"log"
-	"os"
 	"github.com/muesli/termenv"
 	"golang.org/x/term"
 	"unicode/utf8"
 )
-
-const (
-	BufTypeOrigin = iota
-	BufTypeAddition
-)
-
-/// Record represents one piece in PieceTable.
-/// It denote which table to use, first index in table, and length of text
-type Record struct {
-	bufType int
-	startIdx int
-	length int
-}
-
-type PieceTable struct {
-	origin string
-	addition string
-	records []Record
-}
-
-/// Create PieceTable for given file.
-func FromFile(fn string) (PieceTable, error) {
-	data, err := os.ReadFile(fn)
-	if err != nil {
-		return PieceTable{}, err
-	}
-
-	return PieceTable {origin:string(data),
-		addition: "",
-		records: []Record{Record{bufType: BufTypeOrigin, startIdx: 0, length: len(data)}},
-	}, nil
-}
-
-/// Returns contents of given PieceTable.
-func (pt PieceTable) Contents() string {
-	var ret string
-	for i := 0; i < len(pt.records); i++ {
-		record := pt.records[i]
-		switch record.bufType {
-		case BufTypeOrigin:
-			ret += pt.origin[record.startIdx:record.startIdx+record.length]
-		case BufTypeAddition:
-			ret += pt.addition[record.startIdx:record.startIdx+record.length]
-		}
-	}
-	return ret
-}
 
 type EditorState struct {
 	exit bool
