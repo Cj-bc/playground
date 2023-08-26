c5ef767d86ea1b0679828984f7a14e5aa50430c2
Cj-bc <cj.bc-sd@outlook.jp>
2023-08-27 02:35:07 +0900

    Change: go/txteditor> EditorState.CurrentBuffer returns pointer instead of Buffer copy
    Because when I call CurrentBuffer(), most of time I want to modify it.
    Also, I can provide something like 'SetCurrentBuffer' to update it, but
    it might be slow

--- go/txteditor/editor_state.go
+++ go/txteditor/editor_state.go
@@ -24,10 +24,10 @@ func EmptyEditorState() EditorState {
 	return EditorStateWithBuffer(Buffer{pieceTable: EmptyPieceTable(), point: 0})
 }
 
-func (st EditorState) CurrentBuffer() Buffer {
+func (st EditorState) CurrentBuffer() *Buffer {
 	if len(st.buffers) < st.currentBufferIdx {
 		// As this should not occur, I use panic instead of Error
 		panic(fmt.Sprintf("currentBufferIdx out-of-range: %d, but limit is %d", st.currentBufferIdx, len(st.buffers)))
 	}
-	return st.buffers[st.currentBufferIdx]
+	return &st.buffers[st.currentBufferIdx]
 }
