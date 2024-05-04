c6c57e2d34ff01c538e77f6bc4affd42570dd0b2
Cj-bc <cj.bc-sd@outlook.jp>
2024-05-04 18:03:43 +0900

    CHange: haskell/wasm-runtime> Parse wasm module number in preamble

--- haskell/wasm-runtime/src/Parser.hs
+++ haskell/wasm-runtime/src/Parser.hs
@@ -3,11 +3,14 @@ module Parser (preamble) where
 
 import Text.Megaparsec
 import Text.Megaparsec.Byte
+import Text.Megaparsec.Byte.Binary
 import Control.Monad
 import Data.ByteString as BS
 import Data.Void
+import Data.Word (Word32)
 
 type Parser = Parsec Void BS.ByteString
 
-preamble :: Parser ()
-preamble = void $ string "\0asm"
+-- | Parser for WASM preamble
+preamble :: Parser Word32
+preamble = string "\0asm" *> word32le
