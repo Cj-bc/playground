d4c93b609876bdf5be007908885e650835f9555b
Cj-bc <cj.bc-sd@outlook.jp>
2024-10-20 11:01:39 +0900

    feat(dotnet/tryVyaml)> Move contents in a function to write more experiments

--- dotnet/tryVyaml/Program.cs
+++ dotnet/tryVyaml/Program.cs
@@ -1,25 +1,29 @@
 ﻿using VYaml.Serialization;
 
-if (args.Length < 1)
-{
-    Console.WriteLine("usage: EXE UNITY-YAML-FILE");
-    return -1;
-}
-
-string fileName = args[0];
-
-if (!File.Exists(fileName))
-{
-    Console.WriteLine($"fatail: coul'd not find file \"{fileName}\"");
-    return -1;
-}
-
-using var stream = File.OpenRead(args[0]);
-
-foreach (var component in await YamlSerializer.DeserializeMultipleDocumentsAsync<Dictionary<object,object>>(stream))
-{
-    foreach (var (k, v) in component) {
-	Console.WriteLine($"`-- {k}: {v}");
+async Task<int> Experiment_ExploringFileContent(){
+    if (args.Length < 1)
+    {
+	Console.WriteLine("usage: EXE UNITY-YAML-FILE");
+	return -1;
     }
+
+    string fileName = args[0];
+
+    if (!File.Exists(fileName))
+    {
+	Console.WriteLine($"fatail: coul'd not find file \"{fileName}\"");
+	return -1;
+    }
+
+    using var stream = File.OpenRead(args[0]);
+
+    foreach (var component in await YamlSerializer.DeserializeMultipleDocumentsAsync<Dictionary<object,object>>(stream))
+    {
+	foreach (var (k, v) in component) {
+	    Console.WriteLine($"`-- {k}: {v}");
+	}
+    }
+    return 0;
 }
-return 0;
+
+return await Experiment_ExploringFileContent();
