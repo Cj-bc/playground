db533dd1e7bf0178e522e7206735dbd855b07d66
Cj-bc <cj.bc-sd@outlook.jp>
2026-05-06 16:45:57 +0900

    fixup! feat: write eachSystems in flake.nix

--- flix/flake.nix
+++ flix/flake.nix
@@ -16,8 +16,6 @@
           });
         };
       flix_0_71_0 = flix_overlay "0.71.0";
-      pkgs-x86_64 = import nixpkgs { system = "x86_64-linux"; overlays = [ flix_0_71_0 ]; };
-      pkgs-aarch64 = import nixpkgs { system = "aarch64-linux"; overlays = [ flix_0_71_0 ]; };
       # referenced: https://github.com/numtide/flake-utils/blob/11707dc2f618dd54ca8739b309ec4fc024de578b/lib.nix#L33
       eachSystems = systems: op:
         builtins.zipAttrsWith (name: values: builtins.foldl' (acc: v: acc // v) {} values) (builtins.map op systems);
