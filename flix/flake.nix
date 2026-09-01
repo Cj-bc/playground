aea7fb1f6f0de9e49b6473e78059b1eb5c6e1ebc
Cj-bc <cj.bc-sd@outlook.jp>
2026-09-01 05:31:04 +0000

    feat: usevflix v0.75.3

--- flix/flake.nix
+++ flix/flake.nix
@@ -3,24 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flix.url = "github:Cj-bc/flix.nix";
   };
 
-  outputs = { self, nixpkgs }: 
-    let flix_overlay = version: hash: final: prev: {
-          flix = prev.flix.overrideAttrs(finalAttrs: previousAttrs: {
-            version = version;
-            src = prev.fetchurl {
-              url = "https://github.com/flix/flix/releases/download/v${version}/flix.jar";
-              sha256 = hash;
-            };
-          });
-        };
-      flix_0_71_0 = flix_overlay "0.71.0" "sha256-Ha5oRDpQ7YuGsaF/ZNx8b+HjTSroxZEjzI3zR3g7NXI=";
-      # referenced: https://github.com/numtide/flake-utils/blob/11707dc2f618dd54ca8739b309ec4fc024de578b/lib.nix#L33
-      eachSystems = systems: op:
-        builtins.zipAttrsWith (name: values: builtins.foldl' (acc: v: acc // v) {} values) (builtins.map op systems);
+  outputs = { self, nixpkgs, flix }:
+    let eachSystems = systems: op:
+          builtins.zipAttrsWith (name: values: builtins.foldl' (acc: v: acc // v) {} values) (builtins.map op systems);
     in eachSystems ["x86_64-linux" "aarch64-linux"] (system:
-      let pkgs = import nixpkgs { system = system; overlays = [ flix_0_71_0 ]; };
+      let pkgs = import nixpkgs { system = system; overlays = [ flix.overlays.flix_0_75_3 ]; };
       in
         {
           packages.${system}.default = pkgs.flix;
